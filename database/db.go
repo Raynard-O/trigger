713ae90463e06437d6f8d96fae4cb67edd4631d8
raynard <raynardomongbale@gmail.com>
2021-03-02 12:18:06 -0500

    update the Db query

--- database/db.go
+++ database/db.go
@@ -44,13 +44,17 @@ type Tag struct {
 	ID   int    `json:"id"`
 	SensorID sql.NullInt64 `json:"sensor_id"`
 	SampleID	int `json:"sample_id"`
+	Date sql.NullTime `json:"date"`
+	x sql.NullInt64 `json:"x"`
+	y sql.NullInt64 `json:"y"`
+	z sql.NullInt64 `json:"z"`
 }
 
 //DBQuery
 // enter table and close value for query
 func (d *DB) DBQuery(table, row string)  {
 	///results, err := db.Query("SELECT id, name FROM tags")
-	query := fmt.Sprintf("SELECT %v, sample_id FROM %v", row, table)
+	query := fmt.Sprintf("SELECT %v, sample_id, x,y,z FROM %v", row, table)
 	results, err := d.Db.Query(query)
 
 	if err != nil {
@@ -58,33 +62,60 @@ func (d *DB) DBQuery(table, row string)  {
 	}
 	//results.Scan("id", "sensor_id")
 	var last_id int
+	var changeZ,changeY,changeX int
+	var num int
+
 	for results.Next() {
 		var tag Tag
 		// for each row, scan the result into our tag composite object
-		err = results.Scan(&tag.ID, &tag.SampleID)
+		err = results.Scan(&tag.ID, &tag.SampleID, &tag.x, &tag.y, &tag.z)
 
 		if err != nil {
 			panic(err.Error()) // proper error handling instead of panic in your app
 		}
-
+		if tag.SampleID== last_id {
+			num++
+			changeX += int(tag.x.Int64)
+			changeY += int(tag.y.Int64)
+			changeZ += int(tag.z.Int64)
+			//fmt.Println("same values")
+		}
 		if tag.SampleID!= last_id {
-			fmt.Println("changes")
-			fmt.Printf("new id %v, last id : %v\n", tag.SampleID, last_id)
+			if num == 0 {
+				num = 1
+			}
+			newX := changeX/num
+			newY := changeY/num
+			newZ := changeZ/num
+			//fmt.Printf("newX %v,newY %v, newZ %v,changeX : %v, changeY : %v, changeZ : %v, num : %v\n", newX, newY, newZ, changeX,changeY,changeZ,  num)
+			num = 0
+			changeX,changeY,changeZ = 0,0,0
+			table_row := fmt.Sprintf("INSERT INTO operation_statistics (id, sample_id, x_mean, y_mean, z_mean) \nVALUES (%v,%v,%v,%v,%v)", last_id, tag.SampleID, newX,newY,newZ)
+			d.Insert(table_row)
 
 		}
-
+		//("INSERT INTO test VALUES ( 2, 'TEST' )")
 		last_id = tag.SampleID
 	}
 
 }
 
-type Change struct {
-
-}
-
+//"INSERT INTO operation_statistics(id, datetime, sample_id, x_mean, y_mean, z_mean) VALUES ( 2, 'TEST' )"
 //Insert
 //insert data in new database when trigger
 //takes in data change struct
-func Insert(change string)  {
-	fmt.Println(change)
+func (d *DB) Insert(change string)  {
+
+	//perform a db.Query insert
+	insert, err := d.Db.Query(change)
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
+
+
 }
+
